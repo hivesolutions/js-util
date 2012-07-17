25f7937c815c240740e7f03bde592737621b8359
João Magalhães <joamag@hive.pt>
2012-07-17 22:39:16 +0100

    new js util value

--- firebug_redirector.js
+++ firebug_redirector.js
@@ -1,5 +1,5 @@
 // Hive Colony Framework
-// Copyright (C) 2008 Hive Solutions Lda.
+// Copyright (C) 2008-2012 Hive Solutions Lda.
 //
 // This file is part of Hive Colony Framework.
 //
@@ -18,9 +18,9 @@
 
 // __author__    = Tiago Silva <tsilva@hive.pt>
 // __version__   = 1.0.0
-// __revision__  = $LastChangedRevision: 1577 $
-// __date__      = $LastChangedDate: 2009-02-24 12:39:05 +0000 (Ter, 24 Fev 2009) $
-// __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
+// __revision__  = $LastChangedRevision$
+// __date__      = $LastChangedDate$
+// __copyright__ = Copyright (c) 2008-2012 Hive Solutions Lda.
 // __license__   = GNU General Public License (GPL), Version 3
 
 /**
--- select.js
+++ select.js
@@ -1,5 +1,5 @@
 // Hive Colony Framework
-// Copyright (C) 2008 Hive Solutions Lda.
+// Copyright (C) 2008-2012 Hive Solutions Lda.
 //
 // This file is part of Hive Colony Framework.
 //
@@ -18,9 +18,9 @@
 
 // __author__    = João Magalhães <joamag@hive.pt>
 // __version__   = 1.0.0
-// __revision__  = $LastChangedRevision: 9229 $
-// __date__      = $LastChangedDate: 2010-07-08 18:07:05 +0100 (qui, 08 Jul 2010) $
-// __copyright__ = Copyright (c) 2008 Hive Solutions Lda.
+// __revision__  = $LastChangedRevision$
+// __date__      = $LastChangedDate$
+// __copyright__ = Copyright (c) 2008-2012 Hive Solutions Lda.
 // __license__   = GNU General Public License (GPL), Version 3
 
 var Select = Select || {};

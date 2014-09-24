0e4378ae9b7577d61047523ad70db91a635f236d
joamag <joamag@gmail.com>
2014-09-24 10:12:59 +0100

    removed some unused util classes

--- logging/loggy_handler.js
+++ logging/loggy_handler.js
@@ -23,6 +23,8 @@
 // __copyright__ = Copyright (c) 2008-2014 Hive Solutions Lda.
 // __license__   = GNU General Public License (GPL), Version 3
 
+var Logging = Logging || {};
+
 /**
  * Constructor of the class.
  *

34554ab492c7254aab9bfb1cbe27c51836db3aa9
joamag <joamag@hive.pt>
2017-11-10 16:20:36 +0000

    new testing

--- test/base64.js
+++ test/base64.js
@@ -1,15 +1,15 @@
 var assert = require("assert");
-var base = require("../lib/encoding/base64");
+var base64 = require("../lib/encoding/base64");
 
 describe("Base64", function() {
     describe("#encode()", function() {
         it("should do simple things", () => {
-            assert.equal(Base64.encode("hello world"), "aGVsbG8gd29ybGQ=");
+            assert.equal(base64.Base64.encode("hello world"), "aGVsbG8gd29ybGQ=");
         });
     });
     describe("#decode()", function() {
         it("should do simple things", () => {
-            assert.equal(Base64.decode("aGVsbG8gd29ybGQ="), "hello world");
+            assert.equal(base64.Base64.decode("aGVsbG8gd29ybGQ="), "hello world");
         });
     });
 });

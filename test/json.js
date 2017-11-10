c8480c42740e6d1498772c45400e6a513aebc31c
joamag <joamag@hive.pt>
2017-11-10 16:36:10 +0000

    new isglobal trick

---
+++ test/json.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+var json = require("../lib/encoding/json");
+
+describe("Json", function() {
+    describe("#toString()", function() {
+        it("should do simple things", () => {
+            assert.equal(json.Json.toString({hello : "world"}), "{\"hello\":\"world\"}");
+        });
+    });
+});

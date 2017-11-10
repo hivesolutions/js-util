f86c5d4988d58ee449eac2d9404cb621adaee1be
joamag <joamag@gmail.com>
2017-11-10 11:30:37 +0000

    initial simple test

---
+++ test/simple.js
@@ -0,0 +1,21 @@
+console.info(Number);
+
+var assert = require("assert");
+var base = require("../general/number");
+
+describe("Number", function() {
+    describe("#formatMoney()", function() {
+        it("should do simple things", () => {
+            assert.equal(12.23.formatMoney(), "12.23");
+            assert.equal(12.23.formatMoney(1), "12.2");
+            assert.equal(12.23.formatMoney(1, ","), "12,2");
+            assert.equal(1200.23.formatMoney(2, ",", "."), "1.200,23");
+            assert.equal((-1200.23).formatMoney(2, ",", "."), "-1.200,23");
+        });
+        it("should do currency", () => {
+            assert.equal(12.23.formatMoney(2, ".", ",", "USD"), "12.23 USD");
+            assert.equal(12.23.formatMoney(2, ".", ",", "USD", true), "$ 12.23");
+            assert.equal(12.23.formatMoney(1, ",", ".", "USD", true), "$ 12,2");
+        });
+    });
+});

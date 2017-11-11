b7d293c75100d559fb82f54980a20383fa9850c8
joamag <joamag@gmail.com>
2017-11-11 09:09:13 +0000

    small code change

--- test/json.js
+++ test/json.js
@@ -4,7 +4,9 @@ var json = require("../lib/encoding/json");
 describe("Json", function() {
     describe("#toString()", function() {
         it("should do simple things", () => {
-            assert.equal(json.Json.toString({hello : "world"}), "{\"hello\":\"world\"}");
+            assert.equal(json.Json.toString({
+                hello: "world"
+            }), "{\"hello\":\"world\"}");
         });
     });
 });

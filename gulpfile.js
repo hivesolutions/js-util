df5a2831f16ee8233da8ccc78315c10f9a9b8bbf
joamag <joamag@gmail.com>
2017-11-12 23:43:40 +0000

    new package ignore

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,38 @@ const gulp = require("gulp");
 const size = require("gulp-size");
 const count = require("gulp-count");
 const mocha = require("gulp-mocha");
+const uglify = require("gulp-uglifyes");
 const replace = require("gulp-replace");
 const _package = require("./package.json");
+const pump = require("pump");
 
 var paths = {
     scripts: "lib/**/*.js",
     test: "test/**/*.js"
 };
 
-gulp.task("build", function() {
-    return gulp.src(paths.scripts)
-        .pipe(replace("__VERSION__", _package.version))
-        .pipe(size())
-        .pipe(size({
+gulp.task("build", function(cb) {
+    pump([
+        gulp.src(paths.scripts),
+        uglify({
+            mangle: false,
+            ecma: 6
+        }),
+        replace("__VERSION__", _package.version),
+        size(),
+        size({
             gzip: true
-        }))
-        .pipe(gulp.dest("./dist"))
-        .pipe(count("## assets copied"));
+        }),
+        gulp.dest("./dist"),
+        count("## assets copied")
+    ], cb);
 });
 
 gulp.task("test", function() {
     gulp.src(paths.test)
-        .pipe(mocha({reporter: "spec"}));
+        .pipe(mocha({
+            reporter: "spec"
+        }));
 });
 
 gulp.task("default", ["build"]);

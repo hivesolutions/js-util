bb8ab3d858fd83cf846c7b1f1513597fdaf72e0d
joamag <joamag@hive.pt>
2018-04-03 15:07:52 +0100

    new unit testing

--- test/string/string.js
+++ test/string/string.js
@@ -2,6 +2,30 @@ var assert = require("assert");
 require("../../");
 
 describe("String", function() {
+    describe("#trim()", function() {
+        it("should be able to trim a simple string", () => {
+            assert.equal("hello  ".trim(), "hello");
+            assert.equal(" hello  ".trim(), "hello");
+        });
+    });
+    describe("#startsWith()", function() {
+        it("should be able to detect starting on a simple string", () => {
+            assert.equal("hello  ".startsWith("hello"), true);
+            assert.equal("hello  ".startsWith("world"), false);
+        });
+    });
+    describe("#padStart()", function() {
+        it("should be able to pad start a simple string", () => {
+            assert.equal("1".padStart(2, "0"), "01");
+            assert.equal("1".padStart(3, "0"), "001");
+        });
+    });
+    describe("#padEnd()", function() {
+        it("should be able to pad end a simple string", () => {
+            assert.equal("1".padEnd(2, "0"), "10");
+            assert.equal("1".padEnd(3, "0"), "100");
+        });
+    });
     describe("#capitalize()", function() {
         it("should be able to capitalize a simple string", () => {
             assert.equal("hello".capitalize(), "Hello");

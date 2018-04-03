4afecfc51527f6ba1602e3e048d1bf6e8196ea3f
joamag <joamag@hive.pt>
2018-04-03 12:50:21 +0100

    better logging support

--- examples/logging/simple.js
+++ examples/logging/simple.js
@@ -1,11 +1,14 @@
 var util = require("../../");
 
-var logger = util.Logging.getLogger("default", true);
+var logger = util.Logging.getLogger("default", {
+    level: util.Logging.constants.DEBUG
+});
 logger.addHandler(new util.Logging.StreamHandler());
 
 if (util.Logging.ConsolaHandler.isReady()) {
     logger.addHandler(new util.Logging.ConsolaHandler());
 }
 
+logger.debug("Debug Message");
 logger.info("Info Message");
-logger.info("Error Message");
+logger.error("Error Message");

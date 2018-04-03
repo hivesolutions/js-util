96bcea16cd782e21be2dd5c6d8ff5f6829ec03c3
joamag <joamag@hive.pt>
2018-04-03 13:34:06 +0100

    better logging structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
         "semi": ["error", "always"],
         "space-before-function-paren": ["error", "never"],
         "linebreak-style": ["error", "windows"],
-        "mocha/no-exclusive-tests": "error"
+        "mocha/no-exclusive-tests": "error",
+        "no-use-before-define": "off"
     },
     "env": {
         "browser": true,

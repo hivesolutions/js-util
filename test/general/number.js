bf3d09f3e6b5e780ef4c6b8a56c168c8c17ff7dc
joamag <joamag@hive.pt>
2020-04-27 15:42:09 +0100

    Better unit testing

--- test/general/number.js
+++ test/general/number.js
@@ -4,6 +4,7 @@ require("../../");
 describe("Number", function() {
     describe("#formatMoney()", function() {
         it("should do simple things", () => {
+            assert.strictEqual((0.0).formatMoney(), "0.00");
             assert.strictEqual((12.23).formatMoney(), "12.23");
             assert.strictEqual((12.23).formatMoney(1), "12.2");
             assert.strictEqual((12.23).formatMoney(1, ","), "12,2");
@@ -11,6 +12,7 @@ describe("Number", function() {
             assert.strictEqual((-1200.23).formatMoney(2, ",", "."), "-1.200,23");
         });
         it("should format currency", () => {
+            assert.strictEqual((0.0).formatMoney(2, ".", ",", "USD"), "0.00 USD");
             assert.strictEqual((12.23).formatMoney(2, ".", ",", "USD"), "12.23 USD");
             assert.strictEqual((12.23).formatMoney(2, ".", ",", "USD", true), "$ 12.23");
             assert.strictEqual((12.23).formatMoney(1, ",", ".", "USD", true), "$ 12,2");
@@ -21,6 +23,7 @@ describe("Number", function() {
             assert.strictEqual((-12.23).formatMoney(1, ",", ".", "USD", true), "$ -12,2");
         });
         it("should format TWD currency", () => {
+            assert.strictEqual((0.0).formatMoney(2, ".", ",", "TWD"), "0.00 TWD");
             assert.strictEqual((12.23).formatMoney(2, ".", ",", "TWD"), "12.23 TWD");
             assert.strictEqual((12.23).formatMoney(1, ",", ".", "TWD", true), "NT$ 12,2");
             assert.strictEqual((12.23).formatMoney(2, ".", ",", "TWD", true), "NT$ 12.23");

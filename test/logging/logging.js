54c6a543869132210cc084a47f53f11e4341b1f1
João Magalhães <joamag@gmail.com>
2020-11-16 09:33:59 +0000

    Added logging unit testing

--- test/logging/logging.js
+++ test/logging/logging.js
@@ -8,4 +8,49 @@ describe("Logging", function() {
             assert.notStrictEqual(util.Logging.getLogger("default"), undefined);
         });
     });
+
+    describe("#debug()", function() {
+        it("should be able to log a debug", () => {
+            assert.strictEqual(util.Logging.debug("hello world"), undefined);
+            assert.strictEqual(util.Logging.getLogger("default").debug("hello world"), undefined);
+        });
+    });
+
+    describe("#info()", function() {
+        it("should be able to log a info", () => {
+            assert.strictEqual(util.Logging.info("hello world"), undefined);
+            assert.strictEqual(util.Logging.getLogger("default").info("hello world"), undefined);
+        });
+    });
+
+    describe("#warn()", function() {
+        it("should be able to log a warn", () => {
+            assert.strictEqual(util.Logging.warn("hello world"), undefined);
+            assert.strictEqual(util.Logging.getLogger("default").warn("hello world"), undefined);
+        });
+    });
+
+    describe("#warning()", function() {
+        it("should be able to log a warning", () => {
+            assert.strictEqual(util.Logging.warning("hello world"), undefined);
+            assert.strictEqual(util.Logging.getLogger("default").warning("hello world"), undefined);
+        });
+    });
+
+    describe("#error()", function() {
+        it("should be able to log an error", () => {
+            assert.strictEqual(util.Logging.error("hello world"), undefined);
+            assert.strictEqual(util.Logging.getLogger("default").error("hello world"), undefined);
+        });
+    });
+
+    describe("#critical()", function() {
+        it("should be able to log a critical", () => {
+            assert.strictEqual(util.Logging.critical("hello world"), undefined);
+            assert.strictEqual(
+                util.Logging.getLogger("default").critical("hello world"),
+                undefined
+            );
+        });
+    });
 });

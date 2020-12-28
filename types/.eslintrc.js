d7a85a61efd876afa36ba81cf5defc75c8010191
João Magalhães <joamag@gmail.com>
2020-12-28 13:19:17 +0000

    feat: major bump and added ts eslint support

---
+++ types/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    extends: ["hive", "eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"]
+};
